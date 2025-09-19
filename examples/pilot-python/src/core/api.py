46019003fb1d23d31b92b383712a56095784e236
RomainPeter <romain.peter@gmail.com>
2025-09-19 14:53:58 +0200

    test(scenario1): trivial comment change

---
+++ examples/pilot-python/src/core/api.py
@@ -0,0 +1,15 @@
+# examples/pilot-python/src/main.py
+
+def add(a: int, b: int) -> int:
+    """Adds two integers."""
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    """Subtracts two integers."""
+    return a - b
+
+# A function with a comment to test trivial changes
+def multiply(a: int, b: int) -> int:
+    # This is a multiplier
+    # New comment for testing
+    return a * b
